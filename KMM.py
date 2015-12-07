db5bf5361727cd995cb5f1ba3c3cb867f18684ce
ahmed <jawwada@gmail.com>
2015-12-07 21:06:04 +0100

    added machine learning algorithms

--- KMM.py
+++ KMM.py
@@ -1,3 +1,5 @@
+,,,0,,,0
+,0,,0
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec  7 10:49:25 2015
@@ -26,15 +28,17 @@ utils = sn.MMUtils(gps_p,network)
 streets = network.streets
 gps     = gps_p.gps
 
+
+
 '''
-Tesing Code
+    Tesing Code
 '''
+
+
 print streets['LINESTRING'][1].replace('\'','')
 
 line=LineString(streets['LINESTRING'][1])
 
-
-'''
 plt.figure()
 streets['points'].map(lambda x: plt.plot(x[1::2],x[0::2]))
 plt.plot(gps.ix[:,2:4],'+')
@@ -44,6 +48,6 @@ plt.show()
 print gps.ix[:,2:4]
 print streets['LINESTRING'].head()
 
-Tesing Code Finished
+
 '''
-    
+    Tesing Code Finished    
